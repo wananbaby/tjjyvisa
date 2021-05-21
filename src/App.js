04f7cce434ad0b49a65b5e035ca6fe707f1b1b10
wananbaby <sczlzys@icloud.com>
2021-05-21 11:48:38 +0800

    new

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import ReactGA from "react-ga";
+import store from "./redux";
+import Pages from "./pages";
+import { useInitialDataFetch, useScript } from "./hooks";
+import "./assets/styles/index.less";
+
+function App() {
+    ReactGA.initialize("UA-102409527-2");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    useInitialDataFetch();
+    useScript("https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js");
+
+    return (
+        <>
+            <Pages />
+        </>
+    );
+}
+
+export default () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as TuixueHeader } from "./TuixueHeader";
+export { default as VisaStatusTabs } from "./VisaStatusTabs";
+export { default as EmbassySelector } from "./EmbassySelector";
